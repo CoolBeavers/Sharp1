0b400e7181290dea1bd2353f17947dbe77edbdcb
Вадим Бобров <71228131+LoggerNice@users.noreply.github.com>
2021-09-14 21:52:25 +0400

    Update Program.cs

--- Sharp/С-1/Program.cs
+++ Sharp/С-1/Program.cs
@@ -7,19 +7,6 @@ namespace С_1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            Person person = new Person();
-            Console.WriteLine(person.Name);
-<<<<<<< HEAD
->>>>>>> parent of 02ec279 (+Article, Magazin)
-=======
-            Person person = new Person();
-            Console.WriteLine(person.Name);
->>>>>>> parent of 02ec279 (+Article, Magazin)
-=======
->>>>>>> parent of 8ca2806 (asd)
         }
     }
 }
