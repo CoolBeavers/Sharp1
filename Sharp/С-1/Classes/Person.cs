69fa2a3c3e140f66e326d1b0a27c64e06ad5e58f
LoggerNice <71228131+LoggerNice@users.noreply.github.com>
2021-09-14 17:52:54 +0400

    Person
    Класс человека

---
+++ Sharp/С-1/Classes/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace С_1.Classes
+{
+    class Person
+    {
+        private string _name;
+        private string _surname;
+        private DateTime _date;
+    }
+}
