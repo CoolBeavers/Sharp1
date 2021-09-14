264eca30ae6f412c8d19cc9dcd1cafe9405e129b
6ix-button-fighter <sokol192192@mail.ru>
2021-09-14 17:11:42 +0300

    New func
    Good

--- Sharp/С-1/Classes/Person.cs
+++ Sharp/С-1/Classes/Person.cs
@@ -6,8 +6,30 @@ namespace С_1.Classes
 {
     class Person
     {
-        private string _name;
-        private string _surname;
-        private DateTime _date;
+        private string name;
+        private string surname;
+        private DateTime birthday;
+
+        public Person(string nameValue, string surnameValue, DateTime birthdayValue)
+        {
+            name = nameValue;
+            surname = surnameValue;
+            birthday = birthdayValue;
+        }
+        public Person() : this("Иван", "Иванов", new DateTime(2000, 1, 1))
+        {
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+     
     }
 }
--- Sharp/С-1/Program.cs
+++ Sharp/С-1/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using С_1.Classes;
 
 namespace С_1
 {
     class Program
     {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Name);
         }
     }
 }
